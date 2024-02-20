cf508ad099e7bae68b62beeeae3bb68fe1a74a80
whitcr <whitcr@users.noreply.github.com>
2024-02-20 22:54:47 +0000

    Deploy preview for PR 11 🛫

--- pr-preview/pr-11/commonHelpers2.js
+++ pr-preview/pr-11/commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-7b35ba3a.js";function r(){const o=document.querySelector("#exercise-group"),n=document.querySelector("#gallery > .content");for(let e=0;e<10;e++){const t=o.content.cloneNode(!0),c=t.querySelector("p");c.textContent="other "+e,n.appendChild(t)}}r();
+import"./assets/styles-0b6990bc.js";function r(){const o=document.querySelector("#exercise-group"),n=document.querySelector("#gallery > .content");for(let e=0;e<10;e++){const t=o.content.cloneNode(!0),c=t.querySelector("p");c.textContent="other "+e,n.appendChild(t)}}r();
 //# sourceMappingURL=commonHelpers2.js.map
