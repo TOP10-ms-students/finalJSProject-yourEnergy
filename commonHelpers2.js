d14ab6d929077b46478e1a8187128ab6ebc81398
ICxodnik <icxodnik@gmail.com>
2024-02-20 07:55:08 +0000

    Deploying to gh-pages from @ TOP10-ms-students/finalJSProject-yourEnergy@6db8526171dcd556f51f84e188a03c887c815890 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-4eaab0dc.js";function r(){const o=document.querySelector("#exercise-group"),n=document.querySelector("#gallery > .content");for(let e=0;e<10;e++){const t=o.content.cloneNode(!0),c=t.querySelector("p");c.textContent="other "+e,n.appendChild(t)}}r();
+import"./assets/styles-b9a6a62d.js";function r(){const o=document.querySelector("#exercise-group"),n=document.querySelector("#gallery > .content");for(let e=0;e<10;e++){const t=o.content.cloneNode(!0),c=t.querySelector("p");c.textContent="other "+e,n.appendChild(t)}}r();
 //# sourceMappingURL=commonHelpers2.js.map
