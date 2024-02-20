0965cbed84b2738abde05a6695f6bb392cee46d6
ICxodnik <ICxodnik@users.noreply.github.com>
2024-02-20 20:51:53 +0000

    Remove preview for PR 9 🛬

--- pr-preview/pr-9/commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-45617243.js";function r(){const o=document.querySelector("#exercise-group"),n=document.querySelector("#gallery > .content");for(let e=0;e<10;e++){const t=o.content.cloneNode(!0),c=t.querySelector("p");c.textContent="other "+e,n.appendChild(t)}}r();
-//# sourceMappingURL=commonHelpers2.js.map
