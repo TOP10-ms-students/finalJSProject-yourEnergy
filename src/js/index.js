4b73281f27dba2afcfecb3f5a26246613e0c8340
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-02-21 23:44:26 +0200

    galary state and fetch

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,10 @@ import './helper';
 import './quote-of-the-day';
 import './gallery';
 import './services/subscribe-action';
+import './gallery-events';
+
+import { galaryState } from './gallery-events';
+
+galaryState.page = 'Excercises';
 
 // gallery.testTemplate();
