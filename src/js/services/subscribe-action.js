9d422ecda264807e105bfe550a208042aa7e2f0a
Olek Myron <myrondell@gmail.com>
2024-02-21 13:35:57 +0200

    fixed js

--- src/js/services/subscribe-action.js
+++ src/js/services/subscribe-action.js
@@ -1,18 +1,9 @@
+import iziToast from 'izitoast';
+
 import { fetchApi } from './api-service';
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.getElementById('subscribe-form');
-
-iziToast.settings({
-  timeout: 3000,
-  resetOnHover: true,
-  icon: 'material-icons',
-  transitionIn: 'flipInX',
-  transitionOut: 'flipOutX',
-  position: 'topRight',
-});
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
---
+++ src/js/services/third-party-set.js
@@ -0,0 +1,9 @@
+// iziToast settings
+iziToast.settings({
+  timeout: 3000,
+  resetOnHover: true,
+  icon: 'material-icons',
+  transitionIn: 'flipInX',
+  transitionOut: 'flipOutX',
+  position: 'topRight',
+});
