1b5851dd04d39ceb0c1d958833cfabf477b5b679
dm-zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-02-22 14:47:00 +0200

    formatting

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import * as gallery from './services/gallery-service';
 import './helper';
 import './quote-of-the-day';
 import './gallery';
+import './mob-burger-menu';
 import './services/subscribe-action';
 
 // gallery.testTemplate();
