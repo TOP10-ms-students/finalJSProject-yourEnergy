8fa0befbc12efe601076a161679e82598b1c2c59
dm-zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-02-24 16:57:19 +0200

    errors cleaned

--- src/js/mob-burger-menu.js
+++ src/js/mob-burger-menu.js
@@ -2,25 +2,24 @@ const backdrop = document.querySelector('.js-menu-backdrop');
 const mobMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
-const mobMenuLinks = document.querySelectorAll('.js-menu-link');
+const mobMenuLinks = document.querySelectorAll('.js-menu-link, .js-close-menu');
 
-// toggle visibility of the menu backdrop
 function toggleMenu() {
   const isMenuOpen =
-  openMenuBtn
-  .getAttribute('aria-expanded') === true || false;
-  openMenuBtn
-  .setAttribute('aria-expanded', !isMenuOpen);
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
   mobMenu.classList.toggle('is-open');
   backdrop.classList.toggle('is-hidden');
 }
 
-// event listener to toggle menu backdrop onButtons click
 openMenuBtn.addEventListener('click', toggleMenu);
 closeMenuBtn.addEventListener('click', toggleMenu);
 mobMenuLinks.forEach(link => {
   link.addEventListener('click', toggleMenu);
 });
-backdrop.addEventListener('click', ({ target, currentTarget }) => {
-  currentTarget === target && toggleMenu();
+
+backdrop.addEventListener('click', ({ target }) => {
+  if (!target.closest('#mob-menu')) {
+    toggleMenu();
+  }
 });
