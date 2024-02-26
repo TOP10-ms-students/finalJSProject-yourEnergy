ccaf2c373146d1f2a0acad90cb153d7dd5968bb4
AlexMyron <AlexMyron@users.noreply.github.com>
2024-02-26 17:09:44 +0000

    Deploy preview for PR 100 🛫

---
+++ pr-preview/pr-100/commonHelpers.js
@@ -0,0 +1,2 @@
+import{o as s,r as o,i as l}from"./assets/scroll-top-cf278521.js";import"./assets/vendor-ab977035.js";const i=document.querySelector(".js-fav-gallery");function c(e){if(e.target===e.currentTarget)return;const t=e.target;if(t.closest(".ex-item-start")){const a=t.closest(".js-fav-item").id;s(a)}if(t.closest(".ex-item-trash-icon")){const r=t.closest(".js-fav-item");o({_id:r.id}),l()}}i.addEventListener("click",c);
+//# sourceMappingURL=commonHelpers.js.map
